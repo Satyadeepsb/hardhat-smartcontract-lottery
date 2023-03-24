e0cd781f7de46e543eca262acce5258e509ce477
Satyadeepsb <satyadeepsb@gmail.com>
2023-03-24 11:06:15 +0530

    grtRecentWinner function added

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -18,26 +18,43 @@ error Raffle__NotEnoughETHEntered();
 
 contract Raffle is VRFConsumerBaseV2 {
     /* State Variable */
-    uint256 private immutable i_entraceFee;
+    uint256 private immutable i_entranceFee;
     address payable[] private s_players;
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
     bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    uint256 private immutable i_interval;
+
+    /* Lottery Variables */
+    address private s_recentWinner;
 
     /* Events */
     event RaffleEnter(address indexed players);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+
+    /* Functions */
 
     constructor(
         address vrfCoordinatorV2,
-        uint256 entraceFee,
-        bytes32 gasLane
+        uint64 subscriptionId,
+        bytes32 gasLane, // keyHash
+        uint256 interval,
+        uint256 entranceFee,
+        uint32 callbackGasLimit
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
-        i_entraceFee = entraceFee;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
+        i_interval = interval;
+        i_subscriptionId = subscriptionId;
+        i_entranceFee = entranceFee;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
-        if (msg.value < i_entraceFee) {
+        if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
@@ -50,26 +67,39 @@ contract Raffle is VRFConsumerBaseV2 {
         // Request the random number
         // Once we get it, do something with it
         // 2 transaction process
-        // i_vrfCoordinator.requestRandomWords(
-        //     i_gasLane,
-        //     s_subscriptionId,
-        //     requestConfirmations,
-        //     callbackGasLimit,
-        //     numWords
-        // );
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        // Quiz... is this redundant?
+        emit RequestedRaffleWinner(requestId);
     }
 
     function fulfillRandomWords(
         uint256 requestId,
         uint256[] memory randowmWords
-    ) internal override {}
+    ) internal override {
+        // s_players size 20
+        // randomNumber 202
+        // 202 % 10 = 2
+        uint256 indexOfWinner = randowmWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+    }
 
     /* View / Pure Functions  */
     function getEntraceFee() public view returns (uint256) {
-        return i_entraceFee;
+        return i_entranceFee;
     }
 
     function getPlayers(uint256 index) public view returns (address) {
         return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
     }
 }
